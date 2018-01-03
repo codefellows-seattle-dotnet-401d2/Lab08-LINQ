e7ecb73fa0164678b6de80089e0fcdcb5b2eda6f
QueensKid <andres.ibarra0716@gmail.com>
2018-01-03 13:45:22 -0800

    Inittial commit with JSON file data.

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string fileName = "data.json";
+            string path = Path.Combine(Environment.CurrentDirectory, @"LINQ", fileName);
+            string jsonStuff;
+        }
+    }
+
+    
+}
+
---
+++ LINQ/LINQ/Properties.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ
+{
+    public class Geometry
+    {
+        public string Type { get; set; }
+        public List<double> Cordinates { get; set; }
+    }
+
+    public class Properties
+    {
+        public string Zip { get; set; }
+        public string Zity { get; set; }
+        public string State { get; set; }
+        public string Address { get; set; }
+        public string Borough { get; set; }
+        public string Neighborhood { get; set; }
+        public string County { get; set; }
+    }
+
+    public class Feature
+    {
+        public string Type { get; set; }
+        public Geometry Geometry { get; set; }
+        public Properties Properties { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string Type { get; set; }
+        public List<Feature> Features { get; set; }
+    }
+}
+
